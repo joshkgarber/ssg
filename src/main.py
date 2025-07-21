b93c2341c270defce8e1e5645aec99ed24be47c1
Joshua Garber <josh@jkgarber.com>
2025-07-21 19:21:13 +0200

    review

--- src/main.py
+++ src/main.py
@@ -10,21 +10,20 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-basepath = "/"
-if len(sys.argv) == 2:
-    basepath = sys.argv[1]
-if len (sys.argv) > 2:
-    logger.error("Usage: main.py [basepath]")
-    exit(1)
-
-
 dir_path_static = "./static"
 dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
+default_basepath = "/"
 
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
+    if len (sys.argv) > 2:
+        logger.error("Usage: main.py [basepath]")
+        exit(1)
     logger.info("Clearing public")
     if not os.path.exists(dir_path_static):
         raise ValueError("Source folder does not exist")
@@ -34,11 +33,7 @@ def main():
     logger.info("Copying static content from to public")
     copy_files_recursive(dir_path_static, dir_path_public)
     logger.info("Generating content in public")
-    generate_pages_recursive(dir_path_content,
-        template_path,
-        dir_path_public,
-        basepath
-    )
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 if __name__ == "__main__":
